ba5fc42864d0a0530fb7a49705f76f481989c5e8
abishekk92 <bhat.abishek@gmail.com>
2014-03-16 18:18:34 +0530

    Naive classifier based on male/female refs

--- classifier.py
+++ classifier.py
@@ -1,4 +1,10 @@
 import re
+from collections import Counter
+
+MALE_REFERENCES = ['him', 'his', 'he']
+
+FEMALE_REFERENCES = ['her', 'she']
+
 def get_input():
     no_char = int(raw_input())
 
@@ -16,9 +22,15 @@ store = {k:[] for k in characters}
 for line in corpus:
     for char in characters:
         if char in line:
-            store[char].append(line)
+            store[char].append(line.strip())
 
-print store
-#for char in characters:
-#    occurences = re.findall(char+".*$", corpus, re.MULTILINE)
-#    print len(occurences)
+
+for k, v in store.items():
+    line_split = " ".join(v).split(" ")
+    counts = Counter(line_split)
+    male_score = sum(map(lambda _ : counts.get(_,0), MALE_REFERENCES))
+    female_score = sum(map(lambda _ : counts.get(_,0), FEMALE_REFERENCES))
+    if male_score > female_score:
+        print "Male"
+    else:
+        print "Female"
